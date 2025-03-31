2b5efa62e672729afbc90edbfbbf4db4000cd7db
Ammar Eid <Ammarkamall777@gmail.com>
2025-03-31 11:00:44 +0200

    Modify App.jsx to include FavoritesProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
+// src/App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import BookDetailPage from "./pages/BookDetailPage";
+import FavoritesProvider from "./context/FavoritesContext";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/book/:id" element={<BookDetailPage />} />
-      </Routes>
-    </Router>
+    <FavoritesProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/book/:id" element={<BookDetailPage />} />
+        </Routes>
+      </Router>
+    </FavoritesProvider>
   );
 }
 
 export default App;
+
