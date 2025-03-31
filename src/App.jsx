20890710ec16c09319d67fc64fa53dd4d8640e88
Ammar Eid <Ammarkamall777@gmail.com>
2025-03-31 11:14:27 +0200

    Add a Navbar, import and use

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import BookDetailPage from "./pages/BookDetailPage";
 import FavoritesPage from "./pages/FavoritesPage"; // ✅ Import FavoritesPage
-import FavoritesProvider from "./context/FavoritesContext";
+import Navbar from "./components/Navbar"; // ✅ Import Navbar
+import FavoritesProvider from "./context/FavoritesContext"; // ✅ Import FavoritesContext
 
 function App() {
   return (
     <FavoritesProvider>
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/book/:id" element={<BookDetailPage />} />
-          <Route path="/favorites" element={<FavoritesPage />} /> {/* ✅ New Route */}
-        </Routes>
+        <Navbar /> {/* ✅ Add Navbar at the top */}
+        <div className="container mx-auto p-6">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/book/:id" element={<BookDetailPage />} />
+            <Route path="/favorites" element={<FavoritesPage />} /> {/* ✅ New Favorites Route */}
+          </Routes>
+        </div>
       </Router>
     </FavoritesProvider>
   );
