51fe802f12e0f8af8212cb7f1d22a1afa3c07e5f
Ammar Eid <Ammarkamall777@gmail.com>
2025-04-01 22:56:09 +0200

    Import BookList.css in BookCard.jsx and update BookCard.jsx

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -1,13 +1,20 @@
 import { Link } from "react-router-dom";
-// import "../styles/BookCard.css";
-
+import defaultCover  from "../assets/placeholder.jpg";
+import "../styles/BookCard.css";
+import "../styles/BookList.css";
 
 const BookCard = ({ book }) => {
+  // Construct the cover image URL
+  const coverUrl = book.cover_i
+    ? `https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg`
+    : defaultCover; // Placeholder if no cover
+
   return (
     <div className="book-card">
+      <img src={coverUrl} alt={book.title} className="book-cover" />
       <h3 className="text-lg font-semibold">{book.title}</h3>
-      <p className="text-gray-600">by {book.author_name?.join(", ") || "Unknown"}</p>
-      <Link to={`/book/${book.key.replace("/works/", "")}`} className="view" >
+      <p className="text-gray-600">{book.author_name?.join(" & ") || "Unknown author"}</p>
+      <Link to={`/book/${book.key.replace("/works/", "")}`} className="view">
         View Details
       </Link>
     </div>
@@ -15,4 +22,3 @@ const BookCard = ({ book }) => {
 };
 
 export default BookCard;
-
