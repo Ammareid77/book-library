0aa2e54c89e304c79a8b284fae98f8e4ddc0a789
Ammar Eid <Ammarkamall777@gmail.com>
2025-04-02 02:17:42 +0200

    Fix bugs related to the dark mode feature

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom"; // ✅ Import BrowserRouter
+import ThemeProvider from "./context/ThemeContext"; // ✅ Import ThemeProvider
 import App from "./App";
 import './index.css';
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <ThemeProvider>
     <BrowserRouter> {/* ✅ Wrap App inside BrowserRouter */}
       <App />
     </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>
 );
+
+
+
+
